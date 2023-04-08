97be502d24fe68e5ed677a2c7fc8a62c2f03587d
MAKSIM <maksim.ivanod.JR@gmail.com>
2023-04-08 12:28:42 +0300

    Start of the project

---
+++ src/Alphabet.java
@@ -0,0 +1,5 @@
+public class Alphabet {
+    public static final String RusAlphabet = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюя.,\":-!? ";
+    public static final String EngAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz.,\"\":-!? ";
+    public static final String UkrAlphabet = "АБВГҐДЕЄЖЗИІЇЙКЛМНОПРСТУФХЦЧШЩЬЮЯабвгґдеєжзиіїйклмнопрстуфхцчшщьюя.,\"\":-!? ";
+}
---
+++ src/CryptoAnalyzer.java
@@ -0,0 +1,5 @@
+public class CryptoAnalyzer {
+    public static void main(String[] args) {
+
+    }
+}
